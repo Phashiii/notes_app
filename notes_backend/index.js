007490794b1772aa82418b0883ec4508f0cc9756
Thato <thatojakemoema@gmail.com>
2023-10-22 13:16:45 +0200

    notes_backeend

---
+++ notes_backend/index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+
+let notes = [
+    {
+        id: 1,
+        content: "Html is easy", 
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST atre the most importamnt methods of HTTP protocol",
+        important: true
+    },
+    {
+        id: 4,
+        content: "How to steal a millie.",
+        important: true
+    }
+]
+app.get('/', (request,response) => {
+    response.send('<h1>Hello!!</h1><p>Cunty fuck bitch</p>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    
+    if(note){
+        response.json(note)
+    }else{
+        response.status(404).end()
+    }
+  })
+  app.delete('/api/notes/:id' , (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+  })
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+    console.log(body)
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+
+app.put('/api/notes/:id', (request, response) =>{
+  const id = Number(request.params.id)
+  const note = notes.find(n => n.id === id)
+  const changedNote = { ...note, important: !note.important}
+  notes.map(note => note.id === id? note.important = !note.important : note.important)
+  
+  response.send(changedNote)
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`)
+})
